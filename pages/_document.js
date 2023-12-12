d953a634b89b49f821c7c11061b1244165661398
SelamB <selambeyene2@gmail.com>
2023-12-12 15:30:03 -0800

    starts portfolio template

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Navbar from '@/components/Navbar'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {/* page layout that effects ALL urls can be placed here */}
+      <Navbar />
       <body>
         <Main />
         <NextScript />
       </body>
+      <footer>
+        Created by Selam Beyene
+      </footer>
     </Html>
   )
 }
---
+++ pages/about.js
@@ -0,0 +1,43 @@
+import Image from "next/image"
+import styles from "@/styles/About.module.css"
+import { useState } from "react"
+
+export default function About() {
+    const [fontSize, setFontSize] = useState(2)
+    return (
+        <>
+            <h1>About Me</h1>
+            <p className={styles.greenColor}>Im a software engineer that imagines things and makes them</p>
+            {/* <img src="/headshot.png" alt="my headshot"/> */}
+            {/* Below is how you'd use Next.js's Image component */}
+            {/* dynamic css with js values injected into it from state. Can be used for a dark/light button */}
+            <style jsx>
+                {`
+                .fontSize {
+                    font-size: ${fontSize}rem;
+                }
+                `}
+            </style>
+            <p className="fontSize">This is my brand statement</p>
+
+            <button onClick={() => setFontSize(fontSize + 1)}> BIGGER!</button>
+            <button onClick={() => setFontSize(fontSize - 1)}> smaller!</button>
+
+            <Image
+                src="/headshot.png"
+                alt="My Headshot"
+                // if you do use next Image -- hard coded width and height is required (makes responsivness hard)
+                width={200}
+                height={200}
+            />
+            {/* using another hosting service */}
+            {/* will have to go to next.config.js and add the website */}
+            {/* <Image
+                src="/http://placekitten.com/400/600"
+                alt="cat placeholder"
+                width={400}
+                height={600}
+            /> */}
+        </>
+    )
+}

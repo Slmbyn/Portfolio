3a03e922ff66d41c8cee4f6967280bf46935be63
SelamB <selambeyene2@gmail.com>
2024-01-12 17:19:41 -0800

    removes auto scroll on carousel

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,29 +4,22 @@ import { Col, Row } from 'react-bootstrap';
 
 export default function AboutMe() {
   return (
-    <Carousel className="mx-auto" style={{ maxWidth: '1000px' }}>
+    <Carousel className="mx-auto" style={{ maxWidth: '1000px' }} interval={null}>
         <Carousel.Item>
-  <Row className="position-relative">
-    <Image
-      src="beach.jpg"
-      alt="First slide"
-      fluid
-      style={{ width: '100%', height: 'auto', filter: 'brightness(50%)' }}
-      className='mx-auto'
-    />
-    <div className='text-center text-light position-absolute top-50 start-50 translate-middle'>
-      {/* 
-        The 'text-center text-light' classes are added to center the text horizontally and set its color to light.
-        The 'position-absolute top-50 start-50 translate-middle' classes center the text both vertically and horizontally.
-      */}
-      <h3>Fifth slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </div>
-  </Row>
-</Carousel.Item>
-
-
-
+            <Row className="position-relative">
+                <Image
+                src="beach.jpg"
+                alt="First slide"
+                fluid
+                style={{ width: '100%', height: 'auto', filter: 'brightness(50%)' }}
+                className='mx-auto'
+                />
+                <div className='text-center text-light position-absolute top-50 start-50 translate-middle'>
+                    <h3>Fifth slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </Row>
+        </Carousel.Item>
         <Carousel.Item>
             <Row>
                 <Col>

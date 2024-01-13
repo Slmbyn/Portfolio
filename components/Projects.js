9f917bc0bcd6de101dd3be50d92bca7f72688167
SelamB <selambeyene2@gmail.com>
2024-01-12 19:53:53 -0800

    adds condition to project card

--- components/Projects.js
+++ components/Projects.js
@@ -10,8 +10,8 @@ import styles from '../styles/Project.module.css'
 export default function Projects() {
     const [isHovered, setIsHovered] = useState(false)
     const projectInfo = [
-        { title: 'StratTester', description: 'A web application that helps stock traders test and analyze data from various trading strategies.', imageSrc: '/strattester.png', github: 'https://github.com/Slmbyn/strattester', site:'#'},
-        { title: 'PushStart', description: 'PushStart is a full-stack CRUD application that seamlessly connects car buyers & sellers.', imageSrc: '/pushstart.png', github: 'https://github.com/Slmbyn/PushStart', site:'#' },
+        { title: 'StratTester', description: 'A web application that helps stock traders test and analyze data from various trading strategies.', imageSrc: '/strattester.png', github: 'https://github.com/Slmbyn/strattester'},
+        { title: 'PushStart', description: 'PushStart is a full-stack CRUD application that seamlessly connects car buyers & sellers.', imageSrc: '/pushstart.png', github: 'https://github.com/Slmbyn/PushStart'},
         { title: 'Fit-Forecast', description: 'Web application designed to enhance your well-being through personalized workout suggestions based on your local weather conditions.', imageSrc: '/fitforecast.png', github: 'https://github.com/Slmbyn/Fit-Forecast', site:'https://fitforecast-dc33e66f392f.herokuapp.com/' },
         { title: 'Snake', description: 'Snake, the timeless arcade game, is back! In snake, players control a snake with never ending hunger as it navigates through a field in search of food!', imageSrc: '/snake.png', github: 'https://github.com/Slmbyn/Snake', site:'https://snake-gilt-eta.vercel.app/' },
       ];
@@ -35,7 +35,7 @@ export default function Projects() {
                 <Card.Text>{card.description}</Card.Text>
                 <Card.Footer className='text-center bg-dark-subtle'>
                   <Card.Link href={card.github} target="_blank" >Github</Card.Link>
-                  <Card.Link href={card.site} target="_blank" >Visit Site</Card.Link>
+                  {card.site ? <Card.Link href={card.site} target="_blank" >Visit Site</Card.Link> :''}
                 </Card.Footer>
               </Card.Body>
             </Card>
